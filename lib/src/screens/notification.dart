c20febea92f6c4cbf477d2523ef709fae880ef2c
HashimotoEna <8987ena@gmail.com>
2022-10-20 14:07:53 +0900

    appBar追加

--- lib/src/screens/notification.dart
+++ lib/src/screens/notification.dart
@@ -7,10 +7,34 @@ class NotificationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('お知らせ'),
+        title: const Text(
+          '通知',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          icon: CircleAvatar(
+            backgroundColor: Colors.grey,
+            radius: 16,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          )
+        ],
+        backgroundColor: Color.fromARGB(255, 243, 243, 243),
+        elevation: 0.0,
       ),
-      body:
-      const Center(child: Text('お知らせ画面', style: TextStyle(fontSize: 32.0))),
+      body: const Center(
+          child: Text('お知らせ画面',
+              style: TextStyle(
+                fontSize: 32.0,
+              ))),
     );
   }
 }
